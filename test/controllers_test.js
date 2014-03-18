0d809061a572610a5741fedf94669a1ae335fe12
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-03-18 18:31:56 +0000

    Refactors controller tests to use mocks.

--- test/controllers_test.js
+++ test/controllers_test.js
@@ -1,64 +1,40 @@
 describe("Characters controllers test", function() {
-
-  var characters = {
-    all: function() {
-      return [
-        {
-          id: 101,
-          thumbnail: {
-            path: 'foo',
-            extension: 'bar'
-          }
-        },
-        {
-          id: 102,
-          thumbnail: {
-            path: 'baz',
-            extension: 'gah'
-          }
-        }
-      ];
-    },
-    find: function() {
-      return [
-        {
-          thumbnail: {
-            path: 'foo',
-            extension: 'bar'
-          }
-        }
-      ];
-    }
-  };
-
-  var firstCharacter = characters.all()[0];
+  var characters, firstCharacter, scope, controller;
 
   beforeEach(function() {
-    module('marvel.controllers');
-    var self = this;
+    module('marvel.controllers', 'marvel.fixtures');
 
-    inject(function($rootScope, $controller) {
-      self.scope = $rootScope.$new();
-      self.ctrl = $controller('CharactersCtrl', {
-        $scope: self.scope,
+    inject(function($rootScope, $controller, charactersJSON) {
+      characters = {
+        all: function() {
+          return charactersJSON.data.results;
+        },
+        find: function() {
+          return characterJSON.data.results[0];
+        }
+      };
+      firstCharacter = charactersJSON;
+      scope = $rootScope.$new();
+      controller = $controller('CharactersCtrl', {
+        $scope: scope,
         Characters: characters
       });
     });
   });
 
   it("has characters", function() {
-    expect(this.scope.characters.length).toEqual(2);
+    expect(scope.characters.length).toEqual(2);
   });
 
   describe("Current character", function() {
 
     beforeEach(function() {
       spyOn(characters, 'find');
-      this.scope.select(firstCharacter);
+      scope.select(firstCharacter);
     });
 
     it("can select a character", function() {
-      expect(this.scope.currentCharacter).toEqual(firstCharacter);
+      expect(scope.currentCharacter).toEqual(firstCharacter);
     });
 
     // it("fetches the character detail", function() {
@@ -66,7 +42,7 @@ describe("Characters controllers test", function() {
     // });
 
     it("sets the current character detail", function() {
-      expect(this.scope.currentCharacterDetail).toEqual(characters.find());
+      expect(scope.currentCharacterDetail).toEqual(characters.find());
     });
 
   });
@@ -78,19 +54,19 @@ describe("Characters controllers test", function() {
     });
 
     it("paginates next", function() {
-      this.scope.next();
+      scope.next();
       expect(characters.all).toHaveBeenCalledWith({offset: 20});
     });
 
     it("paginates prev", function() {
-      this.scope.offset = 40;
-      this.scope.prev();
+      scope.offset = 40;
+      scope.prev();
       expect(characters.all).toHaveBeenCalledWith({offset: 20});
     });
 
     it("doesn't send negative offset", function() {
-      this.scope.offset = 0;
-      this.scope.prev();
+      scope.offset = 0;
+      scope.prev();
       expect(characters.all).toHaveBeenCalledWith({offset: 0});
     });
 
@@ -103,14 +79,14 @@ describe("Characters controllers test", function() {
     });
 
     it("doesn't send the search term if empty", function() {
-      this.scope.searchTerm = '';
-      this.scope.search();
+      scope.searchTerm = '';
+      scope.search();
       expect(characters.all).toHaveBeenCalledWith({offset: 0});
     });
 
     it("searches for the given string", function() {
-      this.scope.searchTerm = 'cap';
-      this.scope.search();
+      scope.searchTerm = 'cap';
+      scope.search();
       expect(characters.all).toHaveBeenCalledWith({
         offset: 0,
         nameStartsWith: 'cap'
@@ -118,8 +94,8 @@ describe("Characters controllers test", function() {
     });
 
     it("handles pagination with a search term", function() {
-      this.scope.searchTerm = 'cap';
-      this.scope.next();
+      scope.searchTerm = 'cap';
+      scope.next();
       expect(characters.all).toHaveBeenCalledWith({
         offset: 20,
         nameStartsWith: 'cap'
