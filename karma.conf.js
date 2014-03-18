41242cec531041f0ca9121df711fbdfc7eaf6ea5
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-03-18 07:48:08 +0000

    Use text coverage reporter.

--- karma.conf.js
+++ karma.conf.js
@@ -41,6 +41,10 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['dots', 'coverage'],
 
+    coverageReporter: {
+      type: 'text'
+    },
+
 
     // web server port
     port: 9876,
