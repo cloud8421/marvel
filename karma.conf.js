64af8a4dbfa46e1512569fbdd9f0a5f43f8c62b2
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-03-19 13:55:28 +0000

    Exclude features from Karma.

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,8 @@ module.exports = function(config) {
 
     // list of files to exclude
     exclude: [
-      'public/scripts/app.js'
+      'public/scripts/app.js',
+      'test/features/*.js'
     ],
 
 
