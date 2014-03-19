a465a0876ba853e213299658c4cda9911f9bee9a
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-03-19 09:43:07 +0000

    Adds routing to /characters.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,18 @@
-angular.module('marvel', [ 'marvel.controllers', 'marvel.repo', 'marvel.filters' ]);
+var marvelApp = angular.module('marvel', [
+  'ngRoute',
+  'marvel.controllers',
+  'marvel.repo',
+  'marvel.filters'
+]);
+
+marvelApp.config(['$routeProvider',
+  function ($routeProvider) {
+    $routeProvider
+      .when('/characters', {
+        templateUrl: 'partials/characters.html',
+        controller: 'CharactersCtrl'
+      })
+      .otherwise({
+        redirectTo: '/characters'
+      });
+  }]);
